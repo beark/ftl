9a8c4f99fdaf0bbbb9de37c31a047d18d2b3636f
Björn Aili <bjorn.aili@gmail.com>
2013-04-10 21:26:39 +0200

    A single initial type-level function.

---
+++ src/type_functions.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2013 Björn Aili
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ * distribution.
+ */
+#ifndef FTL_TYPELEVEL_H
+#define FTL_TYPELEVEL_H
+
+/*!
+ * \file type_functions.h
+ *
+ * Various type level functions.
+ */
+
+/*!
+ * Top level namespace for the ftl.
+ */
+namespace ftl {
+
+	/**
+	 * Get the basic, "undecorated" return type of a function.
+	 *
+	 * This is really only a composition of std::decay and std::result_of,
+	 * provided for convenience.
+	 */
+	template<typename>
+	struct decayed_result;
+
+	template<typename F, typename...Ps>
+	struct decayed_result<F(Ps...)> {
+		using type =
+			typename std::decay<typename std::result_of<F(Ps...)>::type>::type;
+	};
+
+}
+
+#endif
+
