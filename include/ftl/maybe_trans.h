36f8008a4b36b95ee55bba2b4fbbbf071554c63d
Björn Aili <bjorn.aili@gmail.com>
2013-05-29 22:31:11 +0200

    Improved maybeT docs and added automatic lifting of base monad actions.

--- include/ftl/maybe_trans.h
+++ include/ftl/maybe_trans.h
@@ -46,6 +46,15 @@ namespace ftl {
 	 * maybe monad. The point is to get the functionality of both `M` and
 	 * maybe.
 	 *
+	 * \par Concepts
+	 * - \ref fullycons, if the transformed type is.
+	 * - \ref assignable, as above.
+	 * - \ref deref to the unwrapped transformed type.
+	 * - \ref functor
+	 * - \ref applicative
+	 * - \ref monad
+	 * - \ref foldable, if `M` is Foldable.
+	 *
 	 * \ingroup maybeT
 	 */
 	template<typename M>
@@ -107,6 +116,11 @@ namespace ftl {
 		using concept_parameter = concept_parameter<M>;
 	};
 
+	/**
+	 * `maybeT`'s monad instance.
+	 *
+	 * "Stacks" `M` on top of `maybe`.
+	 */
 	template<typename M>
 	struct monad<maybeT<M>> {
 		/// Shorthand for the concept parameter
@@ -115,9 +129,11 @@ namespace ftl {
 		template<typename U>
 		using M_ = typename re_parametrise<M,U>::type;
 
+		/// Easy reference to maybeT of various parameterisations.
 		template<typename U>
 		using mT = maybeT<M_<U>>;
 
+		/// Uses `M`'s pure to create a value (as opposed to `nothing`)
 		static mT<T> pure(T&& t) {
 			return mT<T>{monad<M_<maybe<T>>>::pure(value(std::forward<T>(t)))};
 		}
@@ -133,22 +149,74 @@ namespace ftl {
 			};
 		}
 
+		/**
+		 * Monadic bind operation.
+		 *
+		 * Binds `M`'s bind operation with an expression that uses `maybe`'s
+		 * bind, to get the composition of the two.
+		 *
+		 * Note that FTL provides automatic lifting of monadic operations, so
+		 * `F` may well be a monadic action in `M`, not just `maybeT<M>`.
+		 */
 		template<
 				typename F,
-				typename U = typename decayed_result<F(T)>::type::T
+				typename U =
+					concept_parameter<typename decayed_result<F(T)>::type>
 		>
 		static mT<U> bind(const mT<T>& m, F&& f) {
-			return mT<U>{
-				*m >>= [f](const maybe<T>& m) {
-					if(m)
-						return *f(*m);
-					else
-						return monad<M_<maybe<U>>>::pure(maybe<U>{});
-				}
-			};
+
+			using monad_t = typename decayed_result<F(T)>::type;
+
+			return bind_helper<monad_t>::bind(m, std::forward<F>(f));
 		}
 
 		static constexpr bool instance = true;
+
+	private:
+		template<typename M2>
+		struct bind_helper {
+			
+			using U = concept_parameter<M2>;
+
+			template<
+					typename F,
+					typename = typename std::enable_if<
+						std::is_same<
+							typename re_parametrise<M,U>::type,
+							M2
+						>::value
+					>
+			>
+			static mT<U> bind(const mT<T>& m, F&& f) {
+				return mT<U>{
+					*m >>= [f](const maybe<T>& m) {
+						if(m)
+							return f(*m);
+						else
+							return monad<M_<maybe<U>>>::pure(maybe<U>{});
+					}
+				};
+			}
+
+		};
+
+		template<typename M2>
+		struct bind_helper<maybeT<M2>> {
+			using U = concept_parameter<M2>;
+
+			template<typename F>
+			static mT<U> bind(const mT<T>& m, F&& f) {
+				return mT<U>{
+					*m >>= [f](const maybe<T>& m) {
+						if(m)
+							return *f(*m);
+						else
+							return monad<M_<maybe<U>>>::pure(maybe<U>{});
+					}
+				};
+			}
+		};
+
 	};
 }
 
